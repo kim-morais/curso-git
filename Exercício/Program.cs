1e163292aa99283acb33d99fc6768ff6494b1874
Kim Morais <kim.morais19@gmail.com>
2019-08-25 13:34:13 -0300

    Exercicio numero 1

---
+++ Exercício/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.Largura = Double.Parse(Console.ReadLine());
+            r.Altura = Double.Parse(Console.ReadLine());
+            Console.WriteLine(r);
+
+        }
+    }
+}
---
+++ Exercício/Retangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercício
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return (2 * Altura) + (2 * Largura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Math.Pow(Altura, 2)) + Math.Pow(Largura, 2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = "
+                + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + " PERÍMETRO = " 
+                + Perimetro().ToString("F2", CultureInfo.InvariantCulture) 
+                + " Diagonal = " 
+                + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
